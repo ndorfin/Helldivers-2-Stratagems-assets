ae9bbcb2b982efb6cf655487f407089db645efe7
Shaun O'Connell <ndorfin@gmail.com>
2024-03-19 23:02:52 +1300

    Fix folder structure

---
+++ mjs/groups.mjs
@@ -0,0 +1,37 @@
+export const GROUPS = [
+	{
+		id: 'bridge',
+		label: 'Bridge',
+		foldername: 'Bridge',
+	},
+	{
+		id: 'engineering_bay',
+		label: 'Engineering Bay',
+		foldername: 'Engineering Bay',
+	},
+	{
+		id: 'general_stratagems',
+		label: 'General Stratagems',
+		foldername: 'General Stratagems',
+	},
+	{
+		id: 'hangar',
+		label: 'Hangar',
+		foldername: 'Hangar',
+	},
+	{
+		id: 'orbital_cannons',
+		label: 'Orbital Cannons',
+		foldername: 'Orbital Cannons',
+	},
+	{
+		id: 'patriotic_administration_center',
+		label: 'Patriotic Administration Center',
+		foldername: 'Patriotic Administration Center',
+	},
+	{
+		id: 'robotics_workshop',
+		label: 'Robotics Workshop',
+		foldername: 'Robotics Workshop',
+	},
+];
